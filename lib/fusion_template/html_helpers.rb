73ff56200817cf5ede2c4402847de0a9b7294b6f
Derek Eder <derek.eder@gmail.com>
2012-11-09 12:42:09 -0600

    grouping CDPH indicators by category

--- lib/fusion_template/html_helpers.rb
+++ lib/fusion_template/html_helpers.rb
@@ -72,35 +72,68 @@ module FusionTemplate
     def current_menu_class(menu_name)
       return "active" if current_menu == menu_name
     end
+
+    def to_dom_id(s)
+      #strip the string
+      ret = s.strip.downcase
+
+      #blow away apostrophes
+      ret.gsub! /['`.]/,""
+
+      # @ --> at, and & --> and
+      ret.gsub! /\s*@\s*/, " at "
+      ret.gsub! /\s*&\s*/, " and "
+
+      #replace all non alphanumeric, underscore or periods with underscore
+       ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+
+       #convert double underscores to single
+       ret.gsub! /_+/,"_"
+
+       #strip off leading/trailing underscore
+       ret.gsub! /\A[_\.]+|[_\.]+\z/,""
+
+       ret
+    end
     
     def atlas_view_modes
-      return ["Birth Rate",
-              "General Fertility Rate",
-              "Low Birth Weight",
-              "Prenatal Care Beginning in First Trimester",
-              "Preterm Births",
-              "Teen Birth Rate",
-              "Assault (Homicide)",
-              "Breast cancer in females",
-              "Cancer (All Sites)",
-              "Colorectal Cancer",
-              "Diabetes-related",
-              "Firearm-related",
-              "Infant Mortality Rate",
-              "Lung Cancer",
-              "Prostate Cancer in Males",
-              "Stroke (Cerebrovascular Disease)",
-              "Childhood Blood Lead Level Screening",
-              "Childhood Lead Poisoning",
-              "Gonorrhea in Females",
-              "Gonorrhea in Males",
-              "Tuberculosis",
-              "Below Poverty Level",
-              "Crowded Housing",
-              "Dependency",
-              "No High School Diploma",
-              "Per Capita Income",
-              "Unemployment"]
+      return {"Births" => [
+                          "Birth Rate",
+                          "General Fertility Rate",
+                          "Low Birth Weight",
+                          "Prenatal Care Beginning in First Trimester",
+                          "Preterm Births",
+                          "Teen Birth Rate"
+                          ],
+              "Deaths" => [
+                          "Assault (Homicide)",
+                          "Breast cancer in females",
+                          "Cancer (All Sites)",
+                          "Colorectal Cancer",
+                          "Diabetes-related",
+                          "Firearm-related",
+                          "Infant Mortality Rate",
+                          "Lung Cancer",
+                          "Prostate Cancer in Males",
+                          "Stroke (Cerebrovascular Disease)"
+                          ],
+              "Lead" => [  
+                          "Childhood Blood Lead Level Screening",
+                          "Childhood Lead Poisoning"
+                        ],
+              "Infectious disease" => [
+                          "Gonorrhea in Females",
+                          "Gonorrhea in Males",
+                          "Tuberculosis"
+                        ],
+              "Demographics" => [
+                          "Below Poverty Level",
+                          "Crowded Housing",
+                          "Dependency",
+                          "No High School Diploma",
+                          "Per Capita Income",
+                          "Unemployment"]
+              }
     end
 
   end
